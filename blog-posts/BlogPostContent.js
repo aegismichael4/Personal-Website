df4714a7cfd2ec284f577b65efb0fa0f57b905cd
aegismichael4 <amichae2@ucsc.edu>
2026-05-17 14:21:51 -0700

    added blog sections (not interactive yet)

--- blog-posts/BlogPostContent.js
+++ blog-posts/BlogPostContent.js
@@ -5,9 +5,24 @@ const urlParams = new URLSearchParams(window.location.search);
 const blogID = urlParams.get('blog');
 
 console.log(blogID);
-document.getElementById(blogID).style.display = 'block';
+const currBlog = document.getElementById(blogID);
+currBlog.style.display = 'block';
 
 const homeButton = document.getElementById("home-button");
 homeButton.addEventListener("click", () => {
     window.location.href = `../index.html`;
 });
+
+//#region blog sections
+
+const blogRegionsContainer = document.getElementById("blog-sections");
+const sections = currBlog.querySelectorAll("h2");
+
+sections.forEach((section) => {
+    const sectionButton = document.createElement("h2");
+    sectionButton.innerHTML = section.innerHTML;
+    console.log(sectionButton.innerHTML);
+    blogRegionsContainer.appendChild(sectionButton);
+});
+
+//#endregion
